49b60702923617e785f27092c2f13308fbee1f26
unknown <Michael>
2022-11-02 15:54:31 +0100

    Commit 1
    - Made Pillar Prefab
    - Made textures to show lenght
    - Made my own scene
    - Combined the pillar prefab stuff

---
+++ Aletta Jacobs/Assets/Michael/Scripts/CanvasLocator.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasLocator : MonoBehaviour
+{
+
+    GameObject canvas;
+    Transform canvasLoc;
+
+    private void Start()
+    {
+        Transform[] children = new Transform[transform.childCount];
+
+        int i = 0;
+        foreach (Transform T in transform)
+            children[i++] = T;
+    }
+
+    private void Update()
+    {
+    }
+}
