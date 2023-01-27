3b5eb6b772fe581b219b23c31124045099726a53
Bloemenpot <mjssmith03@gmail.com>
2023-01-27 12:03:18 +0100

    Camera Clamped nu wel naar beneden. Maar naar boven niet

--- Aletta Jacobs/Assets/Michael/Scripts/CameraDrag.cs
+++ Aletta Jacobs/Assets/Michael/Scripts/CameraDrag.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class CameraDrag : MonoBehaviour
@@ -11,7 +12,16 @@ public class CameraDrag : MonoBehaviour
 
     [SerializeField] public float _moveSpeed;
 
+    [SerializeField] private float camMin, camMax;
+    private float yAngle;
+
     public bool isUsingJoystick = true;
+
+    private void Start()
+    {
+        yAngle = myCamera.transform.rotation.y;
+    }
+
     void FixedUpdate()
     {
 
@@ -24,14 +34,22 @@ public class CameraDrag : MonoBehaviour
             //}
             if (!Input.GetMouseButton(0)) return;
             player.transform.eulerAngles += (_moveSpeed * Time.deltaTime) * new Vector3(0, Input.GetAxis("Mouse X"), 0);
+            
             myCamera.transform.eulerAngles += (_moveSpeed * Time.deltaTime) * new Vector3(-Input.GetAxis("Mouse Y"), 0, 0);
         }
         else
         {
-
-
-            myCamera.transform.eulerAngles -= new Vector3(_joystick.Vertical * (_moveSpeed * Time.deltaTime), 0);
-            player.transform.eulerAngles += new Vector3(0, _joystick.Horizontal * (_moveSpeed * Time.deltaTime));
+            // myCamera.transform.eulerAngles -= new Vector3(Mathf.Clamp(_joystick.Vertical * (_moveSpeed * Time.deltaTime), -70, 70), 0);
+            
+            // player.transform.eulerAngles += new Vector3(0, _joystick.Horizontal * (_moveSpeed * Time.deltaTime));
+            //
+            // float x = Mathf.Clamp(_joystick.Vertical * (_moveSpeed * Time.deltaTime), camMin, camMax);
+            // myCamera.transform.eulerAngles -= new Vector3(x, 0);
+            
+            var angles = myCamera.transform.eulerAngles;
+            var x = angles.x + _joystick.Vertical * (_moveSpeed * Time.deltaTime);
+            x = Mathf.Clamp(x, camMin, camMax);
+            myCamera.transform.eulerAngles = new Vector3(x, angles.y,angles.z);
 
         }
     }
